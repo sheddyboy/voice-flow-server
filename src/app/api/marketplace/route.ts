69fcd0d83650e19f62c8de92eb965a13180e9ffb
personal <shedrackitulah@gmail.com>
2025-02-06 10:27:55 +0100

    feat: implement fetchAllRecords function to retrieve all records from Airtable API

--- src/app/api/marketplace/route.ts
+++ src/app/api/marketplace/route.ts
@@ -4,46 +4,32 @@ import { NextResponse } from "next/server";
 
 export const runtime = "edge";
 export const dynamic = "force-static";
+// export const dynamic = "force-dynamic";
 
 const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY;
 const AIRTABLE_BASE_ID = process.env.AIRTABLE_BASE_ID;
 const TABLE_NAME = "Marketplace";
 
-export async function GET() {
-  try {
-    const response = await fetch(
-      `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/${TABLE_NAME}`,
-      {
-        headers: {
-          Authorization: `Bearer ${AIRTABLE_API_KEY}`,
-        },
-        next: { revalidate: 3600 }, // Cache for 1 hour
-      }
-    );
-
-    if (!response.ok) {
-      throw new Error("Failed to fetch data from Airtable");
-    }
-
-    const data = (await response.json()) as MarketplaceResponse;
-    return NextResponse.json(transformDataToRestructuredData(data));
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    return NextResponse.json(
-      { error: "Internal Server Error" },
-      { status: 500 }
-    );
-  }
-}
-
 // export async function GET() {
 //   try {
-//     const records = await fetchAllRecords();
-//     return NextResponse.json(
-//       transformDataToRestructuredData({ records, offset: "" })
+//     const response = await fetch(
+//       `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/${TABLE_NAME}`,
+//       {
+//         headers: {
+//           Authorization: `Bearer ${AIRTABLE_API_KEY}`,
+//         },
+//         // next: { revalidate: 3600 }, // Cache for 1 hour
+//       }
 //     );
+
+//     if (!response.ok) {
+//       throw new Error("Failed to fetch data from Airtable");
+//     }
+
+//     const data = (await response.json()) as MarketplaceResponse;
+//     return NextResponse.json(transformDataToRestructuredData(data));
 //   } catch (error) {
-//     console.log({ error });
+//     console.error("Error fetching data:", error);
 //     return NextResponse.json(
 //       { error: "Internal Server Error" },
 //       { status: 500 }
@@ -51,32 +37,49 @@ export async function GET() {
 //   }
 // }
 
-// async function fetchAllRecords(): Promise<AirTableRecord[]> {
-//   const allRecords: AirTableRecord[] = [];
-//   let offset: string | undefined = undefined;
+export async function GET() {
+  try {
+    const records = await fetchAllRecords();
+    return NextResponse.json(
+      transformDataToRestructuredData({ records, offset: "" })
+    );
+  } catch (error) {
+    console.log({ error });
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
 
-//   try {
-//     do {
-//       const url = new URL(
-//         `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/${TABLE_NAME}`
-//       );
-//       if (offset) url.searchParams.append("offset", offset);
+async function fetchAllRecords(): Promise<AirTableRecord[]> {
+  const allRecords: AirTableRecord[] = [];
+  let offset: string | undefined = undefined;
 
-//       const response = await fetch(url.toString(), {
-//         headers: { Authorization: `Bearer ${AIRTABLE_API_KEY}` },
-//         next: { revalidate: 3600 }, // Cache for 1 hour
-//       });
+  try {
+    do {
+      const url = new URL(
+        `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/${TABLE_NAME}`
+      );
+      if (offset) url.searchParams.append("offset", offset);
 
-//       if (!response.ok) throw new Error("Failed to fetch data from Airtable");
+      const response = await fetch(url.toString(), {
+        headers: { Authorization: `Bearer ${AIRTABLE_API_KEY}` },
+        next: { revalidate: 3600 }, // Cache for 1 hour
+      });
 
-//       const data: MarketplaceResponse = await response.json();
-//       allRecords.push(...data.records); // Append new records
-//       offset = data.offset; // Get next offset
-//     } while (offset); // Continue fetching until there's no offset
+      if (!response.ok) throw new Error("Failed to fetch data from Airtable");
 
-//     return allRecords;
-//   } catch (error) {
-//     console.error("Error fetching Airtable records:", error);
-//     throw new Error("Failed to retrieve all records");
-//   }
-// }
+      const data: MarketplaceResponse = await response.json();
+      console.log({ records: data.records.length, offset: data.offset });
+      allRecords.push(...data.records); // Append new records
+      console.log({ offset: data.offset });
+      offset = data.offset; // Get next offset
+    } while (offset); // Continue fetching until there's no offset
+
+    return allRecords;
+  } catch (error) {
+    console.error("Error fetching Airtable records:", error);
+    throw new Error("Failed to retrieve all records");
+  }
+}
